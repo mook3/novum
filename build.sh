a2e81bde0828da5fddc43a85642234375bc3d294
Henri Nurmi <henri@nurmi.me>
2024-07-10 02:34:05 +0300

    Add JLCPCB desing rules

--- build.sh
+++ build.sh
@@ -23,6 +23,7 @@ else
 fi
 
 # Step 0: Remove the old build, and install requirements
+log_info "Installing dependencies..."
 if [ ! -f "freerouting.jar" ]; then
     log_info "'freerouting.jar' does not exist. Downloading..."
     wget -O freerouting.jar https://github.com/freerouting/freerouting/releases/download/v1.9.0/freerouting-1.9.0.jar
@@ -36,27 +37,43 @@ if [ -d "out" ]; then
 fi
 
 # Step 1: Run ergogen to generate PCBs and outlines
-log_info "Generating the PCBs"
+log_info "Generating the PCBs..."
 npm run generate
+
+# Step 2: Inject desing rules
+log_info "Adding JLCPCB design rules"
+cp kicad/JLCPCB.kicad_dru "out/$BOARD/pcbs/left.kicad_dru"
+cp kicad/JLCPCB.kicad_dru "out/$BOARD/pcbs/right.kicad_dru"
+cp kicad/JLCPCB.kicad_dru "out/$BOARD/pcbs/switch_plate.kicad_dru"
+cp kicad/JLCPCB.kicad_dru "out/$BOARD/pcbs/bottom_board.kicad_dru"
+$DOCKER_CMD kibot -b out/novum/pcbs/left.kicad_pcb -c novum/kibot/nop.yml
+$DOCKER_CMD kibot -b out/novum/pcbs/right.kicad_pcb -c novum/kibot/nop.yml
+$DOCKER_CMD kibot -b out/novum/pcbs/switch_plate.kicad_pcb -c novum/kibot/nop.yml
+$DOCKER_CMD kibot -b out/novum/pcbs/bottom_board.kicad_pcb -c novum/kibot/nop.yml
+jq '.net_settings.classes[0].clearance |= 0.5' "out/$BOARD/pcbs/left.kicad_pro" > "out/$BOARD/pcbs/left.tmp" && mv "out/$BOARD/pcbs/left.tmp" "out/$BOARD/pcbs/left.kicad_pro"
+jq '.net_settings.classes[0].track_witdh |= 0.3' "out/$BOARD/pcbs/left.kicad_pro" > "out/$BOARD/pcbs/left.tmp" && mv "out/$BOARD/pcbs/left.tmp" "out/$BOARD/pcbs/left.kicad_pro"
+jq '.net_settings.classes[0].clearance |= 0.5' "out/$BOARD/pcbs/right.kicad_pro" > "out/$BOARD/pcbs/right.tmp" && mv "out/$BOARD/pcbs/right.tmp" "out/$BOARD/pcbs/right.kicad_pro"
+jq '.net_settings.classes[0].track_width |= 0.3' "out/$BOARD/pcbs/right.kicad_pro" > "out/$BOARD/pcbs/right.tmp" && mv "out/$BOARD/pcbs/right.tmp" "out/$BOARD/pcbs/right.kicad_pro"
 
 # Step 2: Autoroute the PCB
 mkdir -p "out/$BOARD/freeroute"
 
-log_info "Exporting the DSN files"
+log_info "Exporting the DSN files..."
 $DOCKER_CMD ./kicad/export_dsn.py -b "out/$BOARD/pcbs/left.kicad_pcb" -o "out/$BOARD/freeroute/left.dsn"
 $DOCKER_CMD ./kicad/export_dsn.py -b "out/$BOARD/pcbs/right.kicad_pcb" -o "out/$BOARD/freeroute/right.dsn"
 
-log_info "Routing the left side"
+log_info "Routing the left side..."
 java -jar freerouting.jar -da -dct 0 -de "out/$BOARD/freeroute/left.dsn" -do "out/$BOARD/freeroute/left.ses"
 
-log_info "Routing the right side"
+log_info "Routing the right side..."
 java -jar freerouting.jar -da -dct 0 -de "out/$BOARD/freeroute/right.dsn" -do "out/$BOARD/freeroute/right.ses"
 
-log_info "Importing the SES files"
+log_info "Importing the SES files..."
 $DOCKER_CMD ./kicad/import_ses.py -b "out/$BOARD/pcbs/left.kicad_pcb" -s "out/$BOARD/freeroute/left.ses" -o "out/$BOARD/pcbs/left.kicad_pcb"
 $DOCKER_CMD ./kicad/import_ses.py -b "out/$BOARD/pcbs/right.kicad_pcb" -s "out/$BOARD/freeroute/right.ses" -o "out/$BOARD/pcbs/right.kicad_pcb"
 
-# Step 3: Generate gerbers and images
+# Step 4: Generate gerbers and images
+log_info "Generating gerbers and images..."
 $DOCKER_CMD kibot -b out/novum/pcbs/left.kicad_pcb -c novum/kibot/board.yml
 $DOCKER_CMD kibot -b out/novum/pcbs/right.kicad_pcb -c novum/kibot/board.yml
 $DOCKER_CMD kibot -b out/novum/pcbs/switch_plate.kicad_pcb -c novum/kibot/board.yml
