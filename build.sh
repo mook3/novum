7ba05e40a16eafdf0c63ac9846aad3ee1f542e41
Henri Nurmi <henri@nurmi.me>
2024-07-10 01:18:11 +0300

    Generate gerbers with kibot

--- build.sh
+++ build.sh
@@ -1,38 +1,66 @@
 #!/bin/sh
 
+log_info() {
+    BLUE='\033[0;34m'
+    BOLD='\033[1m'
+    NC='\033[0m'
+    TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
+    echo "${BLUE}${TIMESTAMP}${NC}: ${BOLD}$1${NC}"
+}
+
 BOARD=novum
+
+if [ -f /.dockerenv ]; then
+    DOCKER_CMD=""
+else
+    IMAGE=henkru/ergo-builder:latest
+    DOCKER_CMD="docker run --rm -it --platform linux/amd64 -v $PWD:/kicad-project $IMAGE"
+    # Build the builder if does not exist
+    if [ -z "$(docker images -q $IMAGE 2> /dev/null)" ]; then
+        log_info "Building the builder container"
+        docker build -t $IMAGE builder
+    fi
+fi
 
 # Step 0: Remove the old build, and install requirements
 if [ ! -f "freerouting.jar" ]; then
-  echo "'freerouting.jar' does not exist. Downloading..."
-  wget -O freerouting.jar https://github.com/freerouting/freerouting/releases/download/v1.9.0/freerouting-1.9.0.jar
+    log_info "'freerouting.jar' does not exist. Downloading..."
+    wget -O freerouting.jar https://github.com/freerouting/freerouting/releases/download/v1.9.0/freerouting-1.9.0.jar
 fi
 
 npm install
 
 if [ -d "out" ]; then
-    echo "Removing the 'out' directory..."
+    log_info "Removing the 'out' directory..."
     rm -r "out"
 fi
 
 # Step 1: Run ergogen to generate PCBs and outlines
-echo "Generating the PCBs"
+log_info "Generating the PCBs"
 npm run generate
 
 # Step 2: Autoroute the PCB
 mkdir -p "out/$BOARD/freeroute"
 
-echo "Exporting the DSN files"
-./kicad/export_dsn.py -b "out/$BOARD/pcbs/left.kicad_pcb" -o "out/$BOARD/freeroute/left.dsn"
-./kicad/export_dsn.py -b "out/$BOARD/pcbs/right.kicad_pcb" -o "out/$BOARD/freeroute/right.dsn"
+log_info "Exporting the DSN files"
+$DOCKER_CMD ./kicad/export_dsn.py -b "out/$BOARD/pcbs/left.kicad_pcb" -o "out/$BOARD/freeroute/left.dsn"
+$DOCKER_CMD ./kicad/export_dsn.py -b "out/$BOARD/pcbs/right.kicad_pcb" -o "out/$BOARD/freeroute/right.dsn"
 
-echo "Routing the left side"
+log_info "Routing the left side"
 java -jar freerouting.jar -da -dct 0 -de "out/$BOARD/freeroute/left.dsn" -do "out/$BOARD/freeroute/left.ses"
 
-echo "Routing the right side"
+log_info "Routing the right side"
 java -jar freerouting.jar -da -dct 0 -de "out/$BOARD/freeroute/right.dsn" -do "out/$BOARD/freeroute/right.ses"
 
-echo "Importing the SES files"
-./kicad/import_ses.py -b "out/$BOARD/pcbs/left.kicad_pcb" -s "out/$BOARD/freeroute/left.ses" -o "out/$BOARD/pcbs/left.kicad_pcb"
-./kicad/import_ses.py -b "out/$BOARD/pcbs/right.kicad_pcb" -s "out/$BOARD/freeroute/right.ses" -o "out/$BOARD/pcbs/right.kicad_pcb"
+log_info "Importing the SES files"
+$DOCKER_CMD ./kicad/import_ses.py -b "out/$BOARD/pcbs/left.kicad_pcb" -s "out/$BOARD/freeroute/left.ses" -o "out/$BOARD/pcbs/left.kicad_pcb"
+$DOCKER_CMD ./kicad/import_ses.py -b "out/$BOARD/pcbs/right.kicad_pcb" -s "out/$BOARD/freeroute/right.ses" -o "out/$BOARD/pcbs/right.kicad_pcb"
+
+# Step 3: Generate gerbers and images
+$DOCKER_CMD kibot -b out/novum/pcbs/left.kicad_pcb -c novum/kibot/board.yml
+$DOCKER_CMD kibot -b out/novum/pcbs/right.kicad_pcb -c novum/kibot/board.yml
+$DOCKER_CMD kibot -b out/novum/pcbs/switch_plate.kicad_pcb -c novum/kibot/board.yml
+$DOCKER_CMD kibot -b out/novum/pcbs/bottom_board.kicad_pcb -c novum/kibot/board.yml
+
+log_info "\033[0;32m --- All Done ---\033[0m"
 
