9ff6ec15d3e00315377e6a95b517dcdaecf0a8be
Henri Nurmi <henri@nurmi.me>
2024-07-13 00:15:39 +0300

    Small fixes

--- novum/ergogen/footprints/reset_button.js
+++ novum/ergogen/footprints/reset_button.js
@@ -1,8 +1,7 @@
 module.exports = {
-  nets: {
-  },
   params: {
-    class: "B", // for Button
+    designator: 'B',
+    class: "B",
     side: "F",
     from: { type: 'net', value: 'RST' },
     to: { type: 'net', value: 'GND' },
