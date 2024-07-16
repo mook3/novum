86c620e59634ad68822c99a61f552fdc69c4e262
Henri Nurmi <henri@nurmi.me>
2024-07-16 21:07:41 +0300

    Add logo and version to bottom and plate boards

--- novum/ergogen/footprints/logo.js
+++ novum/ergogen/footprints/logo.js
@@ -1,8 +1,9 @@
 module.exports = {
   params: {
     designator: 'LOGO',
+    mirrored: false,
   },
-  body: (p) => `
+  body: (p) => !p.mirrored ? `
 (footprint "Logo"
 	(version 20240108)
 	(generator "pcbnew")
@@ -274,6 +275,284 @@ module.exports = {
 			(justify left bottom)
 		)
 	)
+)`
+    : `
+(footprint "Logo_Mirrored"
+	(version 20240108)
+	(generator "pcbnew")
+	(generator_version "8.0")
+	(layer "F.Cu")
+  ${p.at}
+	(property "Reference" "${p.ref}"
+		(at 0 0 0)
+		(layer "B.Fab")
+		(hide yes)
+		(uuid "ddc79489-b3a5-4f44-aa94-0509ea567a48")
+		(effects
+			(font
+				(size 1 1)
+				(thickness 0.15)
+			)
+			(justify mirror)
+		)
+	)
+	(property "Value" "Logo_Mirrored"
+		(at 0 1 0)
+		(unlocked yes)
+		(layer "B.Fab")
+		(uuid "1da6241f-0694-43ba-905c-b073d2128ce5")
+		(effects
+			(font
+				(size 1 1)
+				(thickness 0.15)
+			)
+			(justify mirror)
+		)
+	)
+	(property "Footprint" ""
+		(at 0 0 0)
+		(unlocked yes)
+		(layer "B.Fab")
+		(hide yes)
+		(uuid "c7cfd6c0-e04f-4a49-8d35-a880bf6bf8f2")
+		(effects
+			(font
+				(size 1 1)
+				(thickness 0.15)
+			)
+			(justify mirror)
+		)
+	)
+	(property "Datasheet" ""
+		(at 0 0 0)
+		(unlocked yes)
+		(layer "B.Fab")
+		(hide yes)
+		(uuid "95856ba7-a257-4a5a-a54a-05c743516e7f")
+		(effects
+			(font
+				(size 1 1)
+				(thickness 0.15)
+			)
+			(justify mirror)
+		)
+	)
+	(property "Description" ""
+		(at 0 0 0)
+		(unlocked yes)
+		(layer "B.Fab")
+		(hide yes)
+		(uuid "c4f080c7-3ff5-4ca7-8ea7-436652acefd3")
+		(effects
+			(font
+				(size 1 1)
+				(thickness 0.15)
+			)
+			(justify mirror)
+		)
+	)
+	(attr smd)
+	(fp_line
+		(start -6.604 -2.54)
+		(end -2.54 -6.604)
+		(stroke
+			(width 0.5)
+			(type default)
+		)
+		(layer "B.Cu")
+		(uuid "03e81a30-dec0-426f-9577-7dbbd014a442")
+	)
+	(fp_line
+		(start -6.604 2.54)
+		(end -6.604 -2.54)
+		(stroke
+			(width 0.5)
+			(type default)
+		)
+		(layer "B.Cu")
+		(uuid "0babc7cb-fe13-4217-aae7-2b6c7fed99ec")
+	)
+	(fp_line
+		(start -4.699 -0.7112)
+		(end -3.2258 -0.7112)
+		(stroke
+			(width 0.2)
+			(type default)
+		)
+		(layer "B.Cu")
+		(uuid "11f241bb-70c9-4ba9-a558-5eea0776fbbf")
+	)
+	(fp_line
+		(start -3.2258 -0.7112)
+		(end -2.54 -0.0254)
+		(stroke
+			(width 0.2)
+			(type default)
+		)
+		(layer "B.Cu")
+		(uuid "9ee6c8ce-6e17-472b-b4aa-da434030c5b2")
+	)
+	(fp_line
+		(start -2.54 -6.604)
+		(end 2.54 -6.604)
+		(stroke
+			(width 0.5)
+			(type default)
+		)
+		(layer "B.Cu")
+		(uuid "0a9f36b7-1583-4863-91be-5bd1334f7c24")
+	)
+	(fp_line
+		(start -2.54 6.604)
+		(end -6.604 2.54)
+		(stroke
+			(width 0.5)
+			(type default)
+		)
+		(layer "B.Cu")
+		(uuid "6af7ba4a-7591-450b-9c68-ee8c9eb6e60f")
+	)
+	(fp_line
+		(start -2.54 6.604)
+		(end 2.54 6.604)
+		(stroke
+			(width 0.5)
+			(type default)
+		)
+		(layer "B.Cu")
+		(uuid "42f6ae95-d18e-495c-a9b3-8669b16ce811")
+	)
+	(fp_line
+		(start 2.54 6.604)
+		(end 6.604 2.54)
+		(stroke
+			(width 0.5)
+			(type default)
+		)
+		(layer "B.Cu")
+		(uuid "9b1ce5bf-a6ed-4e39-82c7-621409ce23da")
+	)
+	(fp_line
+		(start 3.556 -3.2512)
+		(end 3.556 -1.143)
+		(stroke
+			(width 0.2)
+			(type default)
+		)
+		(layer "B.Cu")
+		(uuid "4ac3a7f9-abff-4cd5-8197-8222b82eb73e")
+	)
+	(fp_line
+		(start 6.604 -2.54)
+		(end 2.54 -6.604)
+		(stroke
+			(width 0.5)
+			(type default)
+		)
+		(layer "B.Cu")
+		(uuid "988a35e4-5be4-45e1-ab72-ab2535512926")
+	)
+	(fp_line
+		(start 6.604 2.54)
+		(end 6.604 -2.54)
+		(stroke
+			(width 0.5)
+			(type default)
+		)
+		(layer "B.Cu")
+		(uuid "dcd20de0-5143-460d-ac2f-7acb05b0dec5")
+	)
+	(fp_arc
+		(start -0.70794 -4.37206)
+		(mid -0.146298 -3.531504)
+		(end 0.050924 -2.54)
+		(stroke
+			(width 0.2)
+			(type default)
+		)
+		(layer "B.Cu")
+		(uuid "c1b0f27b-4659-4692-b75c-1464e19f60b9")
+	)
+	(fp_arc
+		(start 0 2.54)
+		(mid -0.972016 -2.346654)
+		(end 1.796051 1.796051)
+		(stroke
+			(width 0.5)
+			(type default)
+		)
+		(layer "B.Cu")
+		(uuid "35a0c046-7f1a-4216-bc6d-cc81b783e1ec")
+	)
+	(fp_arc
+		(start 3.556 -1.143)
+		(mid 3.242772 -0.307843)
+		(end 2.457594 0.115354)
+		(stroke
+			(width 0.2)
+			(type default)
+		)
+		(layer "B.Cu")
+		(uuid "6476fdbf-1511-4872-b1c2-7e00c732aaef")
+	)
+	(fp_circle
+		(center -4.699 -0.7112)
+		(end -5.022289 -0.7112)
+		(stroke
+			(width 0.2)
+			(type solid)
+		)
+		(fill solid)
+		(layer "B.Cu")
+		(uuid "9de822d1-ff90-4170-b152-8fd77fa92e0a")
+	)
+	(fp_circle
+		(center -0.70794 -4.37206)
+		(end -1.031229 -4.37206)
+		(stroke
+			(width 0.2)
+			(type solid)
+		)
+		(fill solid)
+		(layer "B.Cu")
+		(uuid "f509cb12-2693-4c18-a302-d67f42fce726")
+	)
+	(fp_circle
+		(center 0 0)
+		(end -1.016 0)
+		(stroke
+			(width 0.2)
+			(type solid)
+		)
+		(fill solid)
+		(layer "B.Cu")
+		(uuid "c1155ede-7184-40da-bfc9-9100e9f4de82")
+	)
+	(fp_circle
+		(center 3.556 -3.2512)
+		(end 3.232711 -3.2512)
+		(stroke
+			(width 0.2)
+			(type solid)
+		)
+		(fill solid)
+		(layer "B.Cu")
+		(uuid "9a83f9dc-ccae-4900-97ec-4234b3c28ae0")
+	)
+	(fp_text user "Novum"
+		(at 3.73 4.699 0)
+		(unlocked yes)
+		(layer "B.Cu")
+		(uuid "857ee4f6-47be-445a-98c5-f63b3862df37")
+		(effects
+			(font
+				(size 1.5 1.5)
+				(thickness 0.3)
+				(bold yes)
+			)
+			(justify left bottom mirror)
+		)
+	)
 )
-  `
+`
 };
